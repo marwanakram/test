e7e72f627bfd30f12642225a500eca765c0e26af
marwanakram <55108183+marwanakram@users.noreply.github.com>
2026-04-15 14:21:28 +0200

    Add files via upload

---
+++ open_google.py
@@ -0,0 +1,9 @@
+import webbrowser
+
+# Define the URL
+url = "https://www.google.com"
+
+# Open the URL in the default web browser
+webbrowser.open(url)
+
+print("Opening Google in your default browser...")
