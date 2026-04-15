3dabd10b32ec35922e85e8fada9eb832c111d0f6
marwanakram <55108183+marwanakram@users.noreply.github.com>
2026-04-15 15:32:39 +0200

    Delete open_google.py

--- open_google.py
+++
@@ -1,9 +0,0 @@
-import webbrowser
-
-# Define the URL
-url = "https://www.google.com"
-
-# Open the URL in the default web browser
-webbrowser.open(url)
-
-print("Opening Google in your default browser...")
