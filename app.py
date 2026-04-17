a407ac1917eedd0d15790f6f1896ef1320c591c6
marwanakram <55108183+marwanakram@users.noreply.github.com>
2026-04-17 20:52:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Make sure 'GET' is allowed (it is by default if you don't list any methods)
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    # Important: OpenShift requires 0.0.0.0 and port 8080
     app.run(host='0.0.0.0', port=8080)
