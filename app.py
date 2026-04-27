36d9e05786a5373d8f1213c3c12390ff39b07a33
marwanakram <55108183+marwanakram@users.noreply.github.com>
2026-04-27 14:21:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-
+@app.route('/health')
+def health():
+    # As long as this returns a 200 OK, OpenShift knows the app is healthy
+    return "OK", 200
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
